1ff1a06b4c68571a861035d4f6d34478e568567c
liyasthomas <liyascthomas@gmail.com>
2018-05-18 10:18:49 +0530

    Added service worker & manifest

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+var CACHE_NAME = 'saap-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/aframe.min.js',
+  '/assets/js/aframe-ar.js',
+  '/assets/js/script.js'
+];
+
+self.addEventListener('install', function (event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function (cache) {
+			console.log('Opened cache');
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function (response) {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+
+			// IMPORTANT: Clone the request. A request is a stream and
+			// can only be consumed once. Since we are consuming this
+			// once by cache and once by the browser for fetch, we need
+			// to clone the response.
+			var fetchRequest = event.request.clone();
+
+			return fetch(fetchRequest).then(
+				function (response) {
+					// Check if we received a valid response
+					if (!response || response.status !== 200 || response.type !== 'basic') {
+						return response;
+					}
+
+					// IMPORTANT: Clone the response. A response is a stream
+					// and because we want the browser to consume the response
+					// as well as the cache consuming the response, we need
+					// to clone it so we have two streams.
+					var responseToCache = response.clone();
+
+					caches.open(CACHE_NAME)
+						.then(function (cache) {
+							cache.put(event.request, responseToCache);
+						});
+
+					return response;
+				}
+			);
+		})
+	);
+});
+
+self.addEventListener('activate', function (event) {
+	event.waitUntil(self.clients.claim());
+});
