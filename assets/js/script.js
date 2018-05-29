019c66078934996e26fa986d7560f0ade39ceb23
liyasthomas <liyascthomas@gmail.com>
2018-05-29 07:11:54 +0530

    PWA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,98 @@
-"use strict";
-
 //Generate and Download Screenshot of webpage without lossing the styles https://stackoverflow.com/questions/44494447/generate-and-download-screenshot-of-webpage-without-lossing-the-styles/44495166#44495166
-(function (exports) {
-	function urlsToAbsolute(nodeList) {
-		if (!nodeList.length) {
-			return [];
-		}
-		var attrName = "href";
-		if (nodeList[0].__proto__ === HTMLImageElement.prototype || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
-			attrName = "src";
-		}
-		nodeList = [].map.call(nodeList, function (el, i) {
-			var attr = el.getAttribute(attrName);
-			if (!attr) {
-				return;
-			}
-			var absURL = /^(https?|data):/i.test(attr);
-			if (absURL) {
-				return el;
-			} else {
-				return el;
-			}
-		});
-		return nodeList;
-	}
-
-	function screenshotPage() {
-		var capture = document.getElementById("capture");
-		html2canvas(capture, {
-			onrendered: function (canvas) {
-				canvas.toBlob(function (blob) {
-					saveAs(blob, "Saap.png");
-				});
-			}
-		});
-	}
-
-	function addOnPageLoad_() {
-		window.addEventListener("DOMContentLoaded", function (e) {
-			var scrollX = document.documentElement.dataset.scrollX || 0;
-			var scrollY = document.documentElement.dataset.scrollY || 0;
-			window.scrollTo(scrollX, scrollY);
-		});
-	}
-
-	function generate() {
-		screenshotPage();
-	}
-	exports.screenshotPage = screenshotPage;
-	exports.generate = generate;
-})(window);
+//(function (exports) {
+//	function urlsToAbsolute(nodeList) {
+//		if (!nodeList.length) {
+//			return [];
+//		}
+//		var attrName = "href";
+//		if (nodeList[0].__proto__ === HTMLImageElement.prototype || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
+//			attrName = "src";
+//		}
+//		nodeList = [].map.call(nodeList, function (el, i) {
+//			var attr = el.getAttribute(attrName);
+//			if (!attr) {
+//				return;
+//			}
+//			var absURL = /^(https?|data):/i.test(attr);
+//			if (absURL) {
+//				return el;
+//			} else {
+//				return el;
+//			}
+//		});
+//		return nodeList;
+//	}
+//
+//	function screenshotPage() {
+//		var capture = document.getElementById("capture");
+//		html2canvas(capture, {
+//			onrendered: function (canvas) {
+//				canvas.toBlob(function (blob) {
+//					saveAs(blob, "Saap.png");
+//				});
+//			}
+//		});
+//	}
+//
+//	function addOnPageLoad_() {
+//		window.addEventListener("DOMContentLoaded", function (e) {
+//			var scrollX = document.documentElement.dataset.scrollX || 0;
+//			var scrollY = document.documentElement.dataset.scrollY || 0;
+//			window.scrollTo(scrollX, scrollY);
+//		});
+//	}
+//
+//	function generate() {
+//		screenshotPage();
+//	}
+//	exports.screenshotPage = screenshotPage;
+//	exports.generate = generate;
+//})(window);
 
 //Is it possible to control the camera light on a phone via a website? https://stackoverflow.com/questions/37848494/is-it-possible-to-control-the-camera-light-on-a-phone-via-a-website
 //Test browser support
-const SUPPORTS_MEDIA_DEVICES = "mediaDevices" in navigator;
-
-if (SUPPORTS_MEDIA_DEVICES) {
-	//Get the environment camera (usually the second one)
-	navigator.mediaDevices.enumerateDevices().then(devices => {
-
-		const cameras = devices.filter((device) => device.kind === "videoinput");
-
-		if (cameras.length === 0) {
-			throw "No camera found on this device.";
-		}
-		const camera = cameras[cameras.length - 1];
-
-		// Create stream and get video track
-		navigator.mediaDevices.getUserMedia({
-			video: {
-				deviceId: camera.deviceId,
-				facingMode: ["user", "environment"],
-				height: {
-					ideal: 1080
-				},
-				width: {
-					ideal: 1920
-				}
-			}
-		}).then(stream => {
-			const track = stream.getVideoTracks()[0];
-
-			//Create image capture object and get camera capabilities
-			const imageCapture = new ImageCapture(track)
-			const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-
-				//let there be light!
-				const btn = document.querySelector(".flash");
-				btn.addEventListener("click", function () {
-					var flag = !flag || true;
-					track.applyConstraints({
-						advanced: [{
-							torch: flag
-						}]
-					});
-				});
-			});
-		});
-	});
-}
+//const SUPPORTS_MEDIA_DEVICES = "mediaDevices" in navigator;
+//
+//if (SUPPORTS_MEDIA_DEVICES) {
+//	navigator.mediaDevices.enumerateDevices().then(devices => {
+//
+//		const cameras = devices.filter((device) => device.kind === "videoinput");
+//
+//		if (cameras.length === 0) {
+//			throw "No camera found on this device.";
+//		}
+//		const camera = cameras[cameras.length - 1];
+//
+//		navigator.mediaDevices.getUserMedia({
+//			video: {
+//				deviceId: camera.deviceId,
+//				facingMode: ["user", "environment"],
+//				height: {
+//					ideal: 1080
+//				},
+//				width: {
+//					ideal: 1920
+//				}
+//			}
+//		}).then(stream => {
+//			const track = stream.getVideoTracks()[0];
+//
+//			const imageCapture = new ImageCapture(track)
+//			const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+//
+//				const btn = document.querySelector(".flash");
+//				btn.addEventListener("click", function () {
+//					var flag = !flag || true;
+//					track.applyConstraints({
+//						advanced: [{
+//							torch: flag
+//						}]
+//					});
+//				});
+//			});
+//		});
+//	});
+//}
 
 //Toggle more
 function toggleMore() {
@@ -136,13 +130,51 @@ function toggleScale() {
 	e.classList.toggle("closed");
 }
 
-//Reset
 var p = document.getElementById("capture"),
 	bRange = document.getElementById("b"),
 	sRange = document.getElementById("s"),
 	cRange = document.getElementById("c"),
 	oRange = document.getElementById("o"),
 	zRange = document.getElementById("z");
+
+bRange.addEventListener("input", function () {
+	var sat = sRange.value,
+		con = cRange.value,
+		opa = oRange.value;
+	p.style.filter = "brightness(" + this.value + "%) saturate(" + sat + "%) contrast(" + con + "%) opacity(" + opa + "%)";
+	document.getElementById("bPointSize").value = this.value + "٪";
+}, false);
+
+sRange.addEventListener("input", function () {
+	var bri = bRange.value,
+		con = cRange.value,
+		opa = oRange.value;
+	p.style.filter = "brightness(" + bri + "%) saturate(" + this.value + "%) contrast(" + con + "%) opacity(" + opa + "%)";
+	document.getElementById("sPointSize").value = this.value + "٪";
+}, false);
+
+cRange.addEventListener("input", function () {
+	var bri = bRange.value,
+		sat = sRange.value,
+		opa = oRange.value;
+	p.style.filter = "brightness(" + bri + "%) saturate(" + sat + "%) contrast(" + this.value + "%) opacity(" + opa + "%)";
+	document.getElementById("cPointSize").value = this.value + "٪";
+}, false);
+
+oRange.addEventListener("input", function () {
+	var bri = bRange.value,
+		sat = sRange.value,
+		con = cRange.value;
+	p.style.filter = "brightness(" + bri + "%) saturate(" + sat + "%) contrast(" + con + "%) opacity(" + this.value + "%)";
+	document.getElementById("oPointSize").value = this.value + "٪";
+}, false);
+
+zRange.addEventListener("input", function () {
+	p.style.transform = "scale3d(" + this.value + "," + this.value + "," + this.value + ")";
+	document.getElementById("zPointSize").value = this.value + "×";
+}, false);
+
+//Reset
 
 function resetB() {
 	var sat = sRange.value,
