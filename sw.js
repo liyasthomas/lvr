4184fddd78128bdf863e6639c5a029e3244d087f
liyasthomas <liyascthomas@gmail.com>
2018-05-29 20:14:49 +0530

    Workbox integration

--- sw.js
+++ sw.js
@@ -1,3 +1,53 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+
+if (workbox) {
+	console.log("Yay! Workbox is loaded 🎉");
+} else {
+	console.log("Boo! Workbox didn't load 😬");
+}
+
+workbox.routing.registerRoute(
+	new RegExp('.*\.js'),
+	workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+	// Cache CSS files
+	/.*\.css/,
+	// Use cache but update in the background ASAP
+	workbox.strategies.staleWhileRevalidate({
+		// Use a custom cache name
+		cacheName: 'css-cache',
+	})
+);
+
+workbox.routing.registerRoute(
+	// Cache image files
+	/.*\.(?:png|jpg|jpeg|svg|gif)/,
+	// Use the cache if it's available
+	workbox.strategies.cacheFirst({
+		// Use a custom cache name
+		cacheName: 'image-cache',
+		plugins: [
+      new workbox.expiration.Plugin({
+				// Cache only 20 images
+				maxEntries: 20,
+				// Cache for a maximum of a week
+				maxAgeSeconds: 7 * 24 * 60 * 60,
+			})
+    ],
+	})
+);
+
+workbox.precaching.precacheAndRoute([
+    '/assets/css/style.min.css',
+    '/assets/js/script.min.js',
+	{
+		url: '/index.html',
+		revision: '383676'
+	},
+]);
+
 //This is the "Offline page" service worker
 
 //Install stage sets up the offline page in the cache and opens a new cache
