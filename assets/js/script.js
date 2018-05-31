1ab0c6081f99a9ec23d0486bf0f187e765c9242e
liyasthomas <liyascthomas@gmail.com>
2018-05-31 08:10:52 +0530

    Clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,55 @@
-//Generate and Download Screenshot of webpage without lossing the styles https://stackoverflow.com/questions/44494447/generate-and-download-screenshot-of-webpage-without-lossing-the-styles/44495166#44495166
-//(function (exports) {
-//	function urlsToAbsolute(nodeList) {
-//		if (!nodeList.length) {
-//			return [];
-//		}
-//		var attrName = "href";
-//		if (nodeList[0].__proto__ === HTMLImageElement.prototype || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
-//			attrName = "src";
-//		}
-//		nodeList = [].map.call(nodeList, function (el, i) {
-//			var attr = el.getAttribute(attrName);
-//			if (!attr) {
-//				return;
-//			}
-//			var absURL = /^(https?|data):/i.test(attr);
-//			if (absURL) {
-//				return el;
-//			} else {
-//				return el;
-//			}
-//		});
-//		return nodeList;
-//	}
-//
-//	function screenshotPage() {
-//		var capture = document.getElementById("capture");
-//		html2canvas(capture, {
-//			onrendered: function (canvas) {
-//				canvas.toBlob(function (blob) {
-//					saveAs(blob, "Saap.png");
-//				});
-//			}
-//		});
-//	}
-//
-//	function addOnPageLoad_() {
-//		window.addEventListener("DOMContentLoaded", function (e) {
-//			var scrollX = document.documentElement.dataset.scrollX || 0;
-//			var scrollY = document.documentElement.dataset.scrollY || 0;
-//			window.scrollTo(scrollX, scrollY);
-//		});
-//	}
-//
-//	function generate() {
-//		screenshotPage();
-//	}
-//	exports.screenshotPage = screenshotPage;
-//	exports.generate = generate;
-//})(window);
+//Generate and Download Screenshot of webpage without lossing the styles https://stackoverflow.com/a/44495166
+(function (exports) {
+	function urlsToAbsolute(nodeList) {
+		if (!nodeList.length) {
+			return [];
+		}
+		var attrName = "href";
+		if (nodeList[0].__proto__ === HTMLImageElement.prototype || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
+			attrName = "src";
+		}
+		nodeList = [].map.call(nodeList, function (el, i) {
+			var attr = el.getAttribute(attrName);
+			if (!attr) {
+				return;
+			}
+			var absURL = /^(https?|data):/i.test(attr);
+			if (absURL) {
+				return el;
+			} else {
+				return el;
+			}
+		});
+		return nodeList;
+	}
 
-//Is it possible to control the camera light on a phone via a website? https://stackoverflow.com/questions/37848494/is-it-possible-to-control-the-camera-light-on-a-phone-via-a-website
-//Test browser support
+	function screenshotPage() {
+		var wrapper = document.getElementById("capture");
+		html2canvas(wrapper, {
+			onrendered: function (canvas) {
+				canvas.toBlob(function (blob) {
+					saveAs(blob, "Saap.png");
+				});
+			}
+		});
+	}
+
+	function addOnPageLoad_() {
+		window.addEventListener("DOMContentLoaded", function (e) {
+			var scrollX = document.documentElement.dataset.scrollX || 0;
+			var scrollY = document.documentElement.dataset.scrollY || 0;
+			window.scrollTo(scrollX, scrollY);
+		});
+	}
+
+	function saap() {
+		screenshotPage();
+	}
+	exports.screenshotPage = screenshotPage;
+	exports.saap = saap;
+})(window);
+
+//Is it possible to control the camera light on a phone via a website? https://https://stackoverflow.com/a/47153547
 //const SUPPORTS_MEDIA_DEVICES = "mediaDevices" in navigator;
 //
 //if (SUPPORTS_MEDIA_DEVICES) {
