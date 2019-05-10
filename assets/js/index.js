4d1baa848b54189002117f09dc71d6d5ba268c28
liyasthomas <liyascthomas@gmail.com>
2019-05-11 04:17:13 +0530

    :zap: Updated performance

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,6 @@ for (let i = 0; i < 7; i++) {
 		marker: marker
 	});
 }
-
 var app = new Vue({
 	el: "#app",
 	data: {
@@ -18,18 +17,23 @@ var app = new Vue({
 function resizeCanvas(origCanvas, width, height) {
 	let resizedCanvas = document.createElement("canvas");
 	let resizedContext = resizedCanvas.getContext("2d");
-
 	resizedCanvas.height = height;
 	resizedCanvas.width = width;
-
-	resizedContext.drawImage(origCanvas, 0, 0, width, height);
+	if (width > height) {
+		// Landscape
+		resizedContext.drawImage(origCanvas, 0, 0, width, height);
+	} else {
+		// Portrait
+		var scale = height / width;
+		var scaledHeight = origCanvas.width * scale;
+		var scaledWidth = origCanvas.height * scale;
+		var marginLeft = (origCanvas.width - scaledWidth) / 2;
+		resizedContext.drawImage(origCanvas, marginLeft, 0, scaledWidth, scaledHeight);
+	}
 	return resizedCanvas.toDataURL();
 }
-
 document.getElementById("snap-button").addEventListener("click", function () {
-	let aScene = document
-		.querySelector("a-scene")
-		.components.screenshot.getCanvas("perspective");
+	let aScene = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
 	let frame = captureVideoFrame("video", "png");
 	aScene = resizeCanvas(aScene, frame.width, frame.height);
 	frame = frame.dataUri
